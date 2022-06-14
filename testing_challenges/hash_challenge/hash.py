4eaa0f0f90c3f1fa4fe1efcc8abd3f8bac9e0e32
Ava <st3lla_programming@protonmail.com>
2022-06-13 21:43:14 -0400

    Skeleton folder and hash challenge

---
+++ testing_challenges/hash_challenge/hash.py
@@ -0,0 +1,56 @@
+import hashlib
+try:
+    with open('./flag.txt') as f:
+        flag = f.read()
+except FileNotFoundError:
+    print("Error flag file not found, Contact administrator")
+    exit(-1)
+
+user_string = '''Hash
+[F]lag
+[I]nput
+[E]xit
+'''
+
+def check_flag_hash():
+    while (1):
+        try:
+            first_index = int(input('Start: '))
+            second_index = int(input('End: '))
+        except ValueError:
+            print('Need an actual number')
+            continue
+
+        start = min(first_index, second_index)
+        end = max(second_index, second_index)
+
+        if end - start < 4:
+            print('Need a larger gap')
+            continue
+
+        flag_part = flag[start:end]
+
+        m = hashlib.sha512()
+        m.update(flag_part.encode())
+        flag_hash = m.digest()
+        
+        print(flag_hash)
+        break
+
+def hash_user_input():
+    user_input = input('Enter input to be hashed: ')
+    m = hashlib.sha512()
+    m.update(user_input.encode())
+    hash = m.digest()
+    print(b'Your hash: ' + hash)
+
+while (1):
+    user_input = input(user_string)
+
+    if user_input == 'f':
+        check_flag_hash()
+    elif user_input == 'i':
+        hash_user_input()
+    elif user_input == 'e':
+        exit()
+
