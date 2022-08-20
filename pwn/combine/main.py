8425beea5ef98bf394bf15d776c6c6cd19c5ea03
(j)Avascript <st3lla_programming@protonmail.com>
2022-08-21 02:35:16 +1200

    x.js is not done, but should be done soon

--- pwn/combine/main.py
+++ pwn/combine/main.py
@@ -1,4 +1,4 @@
-import secrets, os, time, threading
+import secrets, os, time, threading, tempfile
 import pow
 
 difficulty = 6
@@ -8,6 +8,7 @@ user_challenge = pow.Pow(hexdigest_size, difficulty, secrets.token_hex().encode(
 user_challenge.create_challenge()
 
 print(f"Arrh, solve me challenge such that sha256({user_challenge.hash} + suffix) ends with {user_challenge.chall}")
+
 user_suffix = input("Your suffix: ")
 
 if len(user_suffix) % 2 != 0:
@@ -18,14 +19,12 @@ if not user_challenge.check_solution(user_suffix):
     print('Wrong suffix')
 
 print('Correct suffix')
-os.system('/spidermonkey_build/mozilla-unified/obj-normal-x86_64-pc-linux-gnu/dist/bin/js')
 
 def call_js():
-    os.system('/spidermonkey_build/mozilla-unified/obj-normal-x86_64-pc-linux-gnu/dist/bin/js')
+    os.system('/spidermonkey_build/mozilla-unified/obj-normal-x86_64-pc-linux-gnu/dist/bin/js -i')
 
 
 t = threading.Thread(target=call_js)
 t.daemon = True
 t.start()
-
 time.sleep(60)
